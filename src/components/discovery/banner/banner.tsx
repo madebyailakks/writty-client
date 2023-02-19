d6644d7bc5454cce244adf6b0d2ddf9018d0e301
Ailakks <hola@cortmnzz.com>
2023-02-19 04:54:31 +0100

    improved post banner design

--- src/components/discovery/banner/banner.tsx
+++ src/components/discovery/banner/banner.tsx
@@ -1,19 +1,16 @@
 import styles from '@/components/discovery/banner/banner.module.css'
-import Sidebar from "@/components/post/sidebar/sidebar";
 
 export default function Banner() {
     return (
-        <div className={styles.wrapper}>
-            <div>
-                <div className={styles.author}>
-                    <p>Ailakks</p>
-                    <p>25 February</p>
-                </div>
-                <h1>Using ChatGPT to build System Diagrams</h1>
-                <p>Interested in Mindmaps — read our blog post on using plantUML with ChatGPT to create awesome Mindmaps The popularity of ChatGPT keeps growing and it’s now being used in a wide range of applications.</p>
-
+        <div className={styles.banner}>
+            <div className={styles.author}>
+                <img className={"profile-avatar"} src={"https://miro.medium.com/freeze/fit/c/176/176/1*pvaJlwES-gx2itutOSb12w.gif"} height={20} />
+                <p>Ailakks</p>
+                <p>25 February</p>
             </div>
-            <Sidebar />
+            <h1>Using ChatGPT to build System Diagrams</h1>
+            <p>Interested in Mindmaps — read our blog post on using plantUML with ChatGPT to create awesome Mindmaps The popularity of ChatGPT keeps growing and it’s now being used in a wide range of applications.</p>
+            <hr />
         </div>
     )
 }
--- src/components/discovery/discovery/discovery.tsx
+++ src/components/discovery/discovery/discovery.tsx
@@ -1,10 +1,12 @@
 import styles from '@/components/discovery/discovery/discovery.module.css'
 import Banner from "@/components/discovery/banner/banner";
+import Sidebar from "@/components/post/sidebar/sidebar";
 
 export default function Discovery() {
     return (
         <div className={styles.wrapper}>
             <Banner />
+            <Sidebar />
         </div>
     )
 }
